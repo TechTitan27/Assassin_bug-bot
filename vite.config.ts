b2b2a182f7cbee29a60b0f074e191de477d154d0
Tech765 <160527169+TechTitan27@users.noreply.github.com>
2025-12-23 06:44:21 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,21 +6,4 @@ export default defineConfig({
   build: {
     outDir: "dist"
   }
-});nv = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
 });
